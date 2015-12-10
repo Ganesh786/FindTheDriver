67861c32af5cf7bf3bede117260475cae59d11ca
Prajoth Antonio <prajothdsa@gmail.com>
2015-12-11 01:57:04 +0530

    “Logs” screen work in progress commit

---
+++ FindTheDriver/Module/Settings/Logs/Custom Cells/LogsCellOneTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  LogsCellOneTableViewCell.m
+//  FindTheDriver
+//
+//  Created by Prajoth Antonio D'sa on 11/12/15.
+//  Copyright © 2015 Endeavour. All rights reserved.
+//
+
+#import "LogsCellOneTableViewCell.h"
+
+@implementation LogsCellOneTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ FindTheDriver/Module/Settings/Logs/LogsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogsViewController.h
+//  FindTheDriver
+//
+//  Created by Prajoth Antonio D'sa on 11/12/15.
+//  Copyright © 2015 Endeavour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogsViewController : UIViewController
+
+@end
