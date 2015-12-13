00e524cf3f72e410201378966f07a568da9ca5db
Ganesh786 <ganesh.korada786@gmail.com>
2015-12-13 14:20:51 +0530

    Tab
    Created the classes for tabs

---
+++ FindTheDriver/Module/Logs/InspectLogs/InspectLogsViewController.h
@@ -0,0 +1,13 @@
+//
+//  InspectLogsViewController.h
+//  FindTheDriver
+//
+//  Created by Ganesh Korada on 13/12/15.
+//  Copyright © 2015 Endeavour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InspectLogsViewController : UIViewController
+
+@end
---
+++ FindTheDriver/Module/Logs/Signature/DriverSignatureViewController.m
@@ -0,0 +1,45 @@
+//
+//  DriverSignatureViewController.m
+//  FindTheDriver
+//
+//  Created by Ganesh Korada on 13/12/15.
+//  Copyright © 2015 Endeavour. All rights reserved.
+//
+
+#import "DriverSignatureViewController.h"
+
+@interface DriverSignatureViewController ()
+
+@end
+
+@implementation DriverSignatureViewController
+
+#pragma mark - View life cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self loadDriverSignatureViewComponents];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - User defined methods
+
+- (void)loadDriverSignatureViewComponents {
+    
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
