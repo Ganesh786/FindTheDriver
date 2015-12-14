0437508c9abbbf9c8cb583704764255267db96d6
Prajoth Antonio <prajothdsa@gmail.com>
2015-12-15 00:17:53 +0530

    Inspect Logs in progress

---
+++ FindTheDriver/Module/Logs/InspectLogs/AddNewDVIRViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddNewDVIRViewController.h
+//  FindTheDriver
+//
+//  Created by Prajoth Antonio D'sa on 14/12/15.
+//  Copyright © 2015 Endeavour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddNewDVIRViewController : UIViewController
+
+@end
---
+++ FindTheDriver/Module/Logs/InspectLogs/AddNewDVIRViewController.m
@@ -0,0 +1,24 @@
+//
+//  AddNewDVIRViewController.m
+//  FindTheDriver
+//
+//  Created by Prajoth Antonio D'sa on 14/12/15.
+//  Copyright © 2015 Endeavour. All rights reserved.
+//
+
+#import "AddNewDVIRViewController.h"
+
+@interface AddNewDVIRViewController ()
+
+@end
+
+@implementation AddNewDVIRViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+@end
