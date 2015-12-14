f20f08f20c8cc2b82cdc6d6425b631ef928ed745
Prajoth Antonio <prajothdsa@gmail.com>
2015-12-15 02:26:54 +0530

    WIP

--- FindTheDriver/Module/Logs/Signature/DriverSignatureViewController.m
+++ FindTheDriver/Module/Logs/Signature/DriverSignatureViewController.m
@@ -21,13 +21,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     [self loadDriverSignatureViewComponents];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - User defined methods
@@ -36,14 +34,5 @@
     [SCUIUtility setLayerForView:_previewLogBtn WithColor:kClearColor];
     [SCUIUtility setLayerForView:_addSignatureBtn WithColor:kClearColor];
 }
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
